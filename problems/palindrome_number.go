143c8b0adda834f46819544b0b701c5987c5e0db
JamieW87 <jamie.w101@gmail.com>
2022-10-14 12:10:23 +0100

    new question

---
+++ problems/palindrome_number.go
@@ -0,0 +1,10 @@
+package problems
+
+//EASY
+//Given an integer x, return true if x is palindrome integer.
+//An integer is a palindrome when it reads the same backward as forward.
+//For example, 121 is a palindrome while 123 is not.
+
+func IsPalindrome(x int) bool {
+
+}
